e381a3c4db05df17d09bcc5d381147c385f88001
pcsin <myscientificindia.singhal@gmail.com>
2016-07-28 01:04:56 +0530

    Fold, filter etc.

--- Modules/hfunctions.erl
+++ Modules/hfunctions.erl
@@ -1,5 +1,5 @@
 -module (hfunctions).
--compile ([export_all, {no_auto_import, [map/2]}]).
+-compile ([export_all, {no_auto_import, [map/2, filter/2]}]).
 
 one () -> 1.
 two () -> 2.
@@ -17,6 +17,27 @@ map ([H|T], F) ->
 add_square (X) ->
     fun (Y) -> X*X + Y*Y
     end.
+
+%% filter on list example
+filter (L, P) ->
+    filter (L, P, []).
+
+filter ([], _, Acc) ->
+    Acc;
+filter ([H|T], P, Acc) ->
+    case P(H) of
+        true -> filter (T, P, [H | Acc]);
+        false -> filter (T, P, Acc)            
+    end.
+
+%% fold example.
+fold (L, P) ->
+    fold (L, P, undefined).
+
+fold ([], _, Acc) ->
+    Acc;
+fold ([H|T], P, Acc) ->
+    fold (T, P, P(Acc, H)).
 
 
 
