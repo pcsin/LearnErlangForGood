444b4178a18cede3c893fc0469926ca5f27ef2a5
pcsin <myscientificindia.singhal@gmail.com>
2016-08-02 01:57:55 +0530

    rpn functional implementation

---
+++ Modules/Problems/rpn.erl
@@ -0,0 +1,24 @@
+-module (rpn).
+-compile (export_all).
+
+read(N) ->
+    case string:to_float (N) of
+         {error, no_float} -> list_to_integer (N);
+         {F, _} -> F    
+    end.
+
+fold ([], _F, Start) ->
+    Start;
+fold ([H|T], F, Start) ->
+    fold (T, F, F(H, Start)).
+
+accumulate ("+", [V1, V2 | S]) -> [V1 + V2 | S];
+accumulate ("-", [V1, V2 | S]) -> [V2 - V1 | S];
+accumulate ("/", [V1, V2 | S]) -> [V2 / V1 | S];
+accumulate ("*", [V1, V2 | S]) -> [V1 * V2 | S];
+accumulate (Val, Stack) ->
+   try read (Val) of
+       N -> [N | Stack]
+   catch 
+       error: _Exception -> {badinput, "Illegal RPN notation."}
+   end.
